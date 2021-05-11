0a55d6de2a2c0dad2f9f15d17a85882529097be5
Nguyen Dat <datnt@dts.com.vn>
2021-05-11 01:59:03 +0000

    CRUD Account

--- src/main/java/dts/com/vn/controller/UserController.java
+++ src/main/java/dts/com/vn/controller/UserController.java
@@ -4,21 +4,22 @@ import dts.com.vn.entities.Account;
 import dts.com.vn.enumeration.ApiResponseStatus;
 import dts.com.vn.enumeration.ErrorCode;
 import dts.com.vn.exception.RestApiException;
+import dts.com.vn.response.AccountResponse;
 import dts.com.vn.response.ApiResponse;
 import dts.com.vn.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
-    @Autowired
     private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/getAllUser")
     public ResponseEntity<ApiResponse> getAllUser() {
@@ -35,4 +36,34 @@ public class UserController {
         return ResponseEntity.ok().body(response);
     }
 
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> add(@RequestBody Account request) {
+        ApiResponse response;
+        try {
+            Account account = userService.add(request);
+            response = new ApiResponse(ApiResponseStatus.SUCCESS.getValue(), account);
+        } catch (RestApiException ex) {
+            response = new ApiResponse(ex);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            response = new ApiResponse(ex, ErrorCode.API_FAILED_UNKNOWN);
+        }
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping("/find-by-id/{id}")
+    public ResponseEntity<ApiResponse> findById(@PathVariable(name = "id", required = true) Long id) {
+        ApiResponse response;
+        try {
+            Account entity = userService.findById(id);
+            AccountResponse accountResponse = new AccountResponse(entity);
+            response = new ApiResponse(ApiResponseStatus.SUCCESS.getValue(), accountResponse);
+        } catch (RestApiException ex) {
+            response = new ApiResponse(ex);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            response = new ApiResponse(ex, ErrorCode.API_FAILED_UNKNOWN);
+        }
+        return ResponseEntity.ok().body(response);
+    }
 }
--- src/main/java/dts/com/vn/service/UserService.java
+++ src/main/java/dts/com/vn/service/UserService.java
@@ -1,19 +1,50 @@
 package dts.com.vn.service;
 
 import dts.com.vn.entities.Account;
+import dts.com.vn.enumeration.ErrorCode;
+import dts.com.vn.exception.RestApiException;
+import dts.com.vn.repository.AccountRepository;
 import dts.com.vn.repository.UserRepository;
+import dts.com.vn.response.AccountResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UserService {
-    @Autowired
+    private AccountRepository accountRepository;
+
     private UserRepository userRepository;
 
+    @Autowired
+    public UserService(AccountRepository accountRepository, UserRepository userRepository) {
+        this.accountRepository = accountRepository;
+        this.userRepository = userRepository;
+    }
+
+    private PasswordEncoder passwordEncoder;
+
     public List<Account> getAllUser() {
-        List<Account> list = userRepository.findAll();
+        List<Account> list = accountRepository.findAll();
         return list;
+    }
+
+
+    public Account add(Account request) {
+        Account account = accountRepository.getAccountByUsername(request.getUsername());
+        if (account == null){
+            this.passwordEncoder = new BCryptPasswordEncoder();
+            request.setPassword(this.passwordEncoder.encode(request.getPassword()));
+            return accountRepository.save(request);
+        }
+        return null;
+    }
+
+    public Account findById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RestApiException(ErrorCode.API_FAILED_UNKNOWN));
     }
 }

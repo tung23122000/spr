23b8470f5d49c372654f373e206d698125daff8e
giangdh <giangdh@dts.com.vn>
2021-05-14 15:56:11 +0700

    Update format

--- src/main/java/dts/com/vn/controller/ServiceProgramController.java
+++ src/main/java/dts/com/vn/controller/ServiceProgramController.java
@@ -24,128 +24,128 @@ import java.util.function.Function;
 @RequestMapping("/api/service-program")
 public class ServiceProgramController {
 
-    @Autowired
-    private ServiceProgramService serviceProgramService;
+	@Autowired
+	private ServiceProgramService serviceProgramService;
 
-    @GetMapping("/find-all")
-    public ResponseEntity<ApiResponse> findAll(
-            @RequestParam(name = "search", required = false) String search, Pageable pageable) {
-        ApiResponse response = null;
-        try {
-            Page<ServiceProgram> page = serviceProgramService.findAll(search, pageable);
-            Page<ServiceProgramResponse> pageResponse =
-                    page.map(new Function<ServiceProgram, ServiceProgramResponse>() {
-                        @Override
-                        public ServiceProgramResponse apply(ServiceProgram service) {
-                            return new ServiceProgramResponse(service);
-                        }
-                    });
-            response = new ApiResponse(ApiResponseStatus.SUCCESS.getValue(), pageResponse);
-        } catch (Exception e) {
-            response = new ApiResponse(e, ErrorCode.API_FAILED_UNKNOWN);
-        }
-        return ResponseEntity.ok().body(response);
-    }
+	@GetMapping("/find-all")
+	public ResponseEntity<ApiResponse> findAll(
+			@RequestParam(name = "search", required = false) String search, Pageable pageable) {
+		ApiResponse response = null;
+		try {
+			Page<ServiceProgram> page = serviceProgramService.findAll(search, pageable);
+			Page<ServiceProgramResponse> pageResponse =
+					page.map(new Function<ServiceProgram, ServiceProgramResponse>() {
+						@Override
+						public ServiceProgramResponse apply(ServiceProgram service) {
+							return new ServiceProgramResponse(service);
+						}
+					});
+			response = new ApiResponse(ApiResponseStatus.SUCCESS.getValue(), pageResponse);
+		} catch (Exception e) {
+			response = new ApiResponse(e, ErrorCode.API_FAILED_UNKNOWN);
+		}
+		return ResponseEntity.ok().body(response);
+	}
 
-    @PostMapping("/add")
-    public ResponseEntity<ApiResponse> add(@RequestBody AddServiceProgramRequest request) {
-        ApiResponse response;
-        try {
-            ServiceProgram data = serviceProgramService.add(request);
-            ServiceProgramResponse responseEntity = new ServiceProgramResponse(data);
-            response = new ApiResponse(ApiResponseStatus.SUCCESS.getValue(), responseEntity);
-        } catch (RestApiException ex) {
-            response = new ApiResponse(ex);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            response = new ApiResponse(ex, ErrorCode.API_FAILED_UNKNOWN);
-        }
-        return ResponseEntity.ok().body(response);
-    }
+	@PostMapping("/add")
+	public ResponseEntity<ApiResponse> add(@RequestBody AddServiceProgramRequest request) {
+		ApiResponse response;
+		try {
+			ServiceProgram data = serviceProgramService.add(request);
+			ServiceProgramResponse responseEntity = new ServiceProgramResponse(data);
+			response = new ApiResponse(ApiResponseStatus.SUCCESS.getValue(), responseEntity);
+		} catch (RestApiException ex) {
+			response = new ApiResponse(ex);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			response = new ApiResponse(ex, ErrorCode.API_FAILED_UNKNOWN);
+		}
+		return ResponseEntity.ok().body(response);
+	}
 
-    @PutMapping("/update")
-    public ResponseEntity<ApiResponse> update(@RequestBody AddServiceProgramRequest request) {
-        ApiResponse response;
-        try {
-            ServiceProgram data = serviceProgramService.update(request);
-            ServiceProgramResponse responseEntity = new ServiceProgramResponse(data);
-            response = new ApiResponse(ApiResponseStatus.SUCCESS.getValue(), responseEntity);
-        } catch (RestApiException ex) {
-            response = new ApiResponse(ex);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            response = new ApiResponse(ex, ErrorCode.API_FAILED_UNKNOWN);
-        }
-        return ResponseEntity.ok().body(response);
-    }
+	@PutMapping("/update")
+	public ResponseEntity<ApiResponse> update(@RequestBody AddServiceProgramRequest request) {
+		ApiResponse response;
+		try {
+			ServiceProgram data = serviceProgramService.update(request);
+			ServiceProgramResponse responseEntity = new ServiceProgramResponse(data);
+			response = new ApiResponse(ApiResponseStatus.SUCCESS.getValue(), responseEntity);
+		} catch (RestApiException ex) {
+			response = new ApiResponse(ex);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			response = new ApiResponse(ex, ErrorCode.API_FAILED_UNKNOWN);
+		}
+		return ResponseEntity.ok().body(response);
+	}
 
-    @GetMapping("/find-by-id/{id}")
-    public ResponseEntity<ApiResponse> findById(@PathVariable(name = "id", required = true) Long id) {
-        ApiResponse response;
-        try {
-            ServiceProgram servicePackage = serviceProgramService.findById(id);
-            ServiceProgramResponse responseEntity = new ServiceProgramResponse(servicePackage);
-            response = new ApiResponse(ApiResponseStatus.SUCCESS.getValue(), responseEntity);
-        } catch (RestApiException ex) {
-            response = new ApiResponse(ex);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            response = new ApiResponse(ex, ErrorCode.API_FAILED_UNKNOWN);
-        }
-        return ResponseEntity.ok().body(response);
-    }
+	@GetMapping("/find-by-id/{id}")
+	public ResponseEntity<ApiResponse> findById(@PathVariable(name = "id", required = true) Long id) {
+		ApiResponse response;
+		try {
+			ServiceProgram servicePackage = serviceProgramService.findById(id);
+			ServiceProgramResponse responseEntity = new ServiceProgramResponse(servicePackage);
+			response = new ApiResponse(ApiResponseStatus.SUCCESS.getValue(), responseEntity);
+		} catch (RestApiException ex) {
+			response = new ApiResponse(ex);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			response = new ApiResponse(ex, ErrorCode.API_FAILED_UNKNOWN);
+		}
+		return ResponseEntity.ok().body(response);
+	}
 
-    @GetMapping("/find-by-package-id/{packageId}")
-    public ResponseEntity<ApiResponse> findByPackageId(
-            @PathVariable(name = "packageId", required = true) Long packageId, Pageable pageable) {
-        ApiResponse response;
-        try {
-            DetailServicePackageResponse entity =
-                    serviceProgramService.findByPackageId(packageId, pageable);
-            response = new ApiResponse(ApiResponseStatus.SUCCESS.getValue(), entity);
-        } catch (RestApiException ex) {
-            response = new ApiResponse(ex);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            response = new ApiResponse(ex, ErrorCode.API_FAILED_UNKNOWN);
-        }
-        return ResponseEntity.ok().body(response);
-    }
+	@GetMapping("/find-by-package-id/{packageId}")
+	public ResponseEntity<ApiResponse> findByPackageId(@PathVariable(name = "packageId") Long packageId, Pageable pageable) {
+		ApiResponse response;
+		try {
+			DetailServicePackageResponse entity =
+					serviceProgramService.findByPackageId(packageId, pageable);
+			response = new ApiResponse(ApiResponseStatus.SUCCESS.getValue(), entity);
+		} catch (RestApiException ex) {
+			response = new ApiResponse(ex);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			response = new ApiResponse(ex, ErrorCode.API_FAILED_UNKNOWN);
+		}
+		return ResponseEntity.ok().body(response);
+	}
 
-    @GetMapping("/detail-service-program/{programId}")
-    public ResponseEntity<ApiResponse> detailServiceProgram(
-            @PathVariable(name = "programId", required = true) Long programId,
-            @Qualifier(value = "pageableIN") Pageable pageableIN,
-            @Qualifier(value = "pageableBILLING") Pageable pageableBILLING,
-            @Qualifier(value = "pageablePCRF") Pageable pageablePCRF,
-            @Qualifier(value = "pageServiceInfo") Pageable pageServiceInfo) {
-        ApiResponse response;
-        try {
-            DetailServiceProgramResponse entity = serviceProgramService.detailServiceProgram(programId,
-                    pageableIN, pageableBILLING, pageablePCRF, pageServiceInfo);
-            response = new ApiResponse(ApiResponseStatus.SUCCESS.getValue(), entity);
-        } catch (RestApiException ex) {
-            response = new ApiResponse(ex);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            response = new ApiResponse(ex, ErrorCode.API_FAILED_UNKNOWN);
-        }
-        return ResponseEntity.ok().body(response);
-    }
+	@GetMapping("/detail-service-program/{programId}")
+	public ResponseEntity<ApiResponse> detailServiceProgram(
+			@PathVariable(name = "programId", required = true) Long programId,
+			@Qualifier(value = "pageableIN") Pageable pageableIN,
+			@Qualifier(value = "pageableBILLING") Pageable pageableBILLING,
+			@Qualifier(value = "pageablePCRF") Pageable pageablePCRF,
+			@Qualifier(value = "pageServiceInfo") Pageable pageServiceInfo) {
+		ApiResponse response;
+		try {
+			DetailServiceProgramResponse entity = serviceProgramService.detailServiceProgram(programId,
+					pageableIN, pageableBILLING, pageablePCRF, pageServiceInfo);
+			response = new ApiResponse(ApiResponseStatus.SUCCESS.getValue(), entity);
+		} catch (RestApiException ex) {
+			response = new ApiResponse(ex);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			response = new ApiResponse(ex, ErrorCode.API_FAILED_UNKNOWN);
+		}
+		return ResponseEntity.ok().body(response);
+	}
 
-    @PostMapping("/clone")
-    public ResponseEntity<ApiResponse> clone(@RequestBody CloneServiceProgramRequest request) {
-        ApiResponse response;
-        try {
-            ServiceProgram data = serviceProgramService.clone(request);
-            ServiceProgramResponse responseEntity = new ServiceProgramResponse(data);
-            response = new ApiResponse(ApiResponseStatus.SUCCESS.getValue(), responseEntity);
-        } catch (RestApiException ex) {
-            response = new ApiResponse(ex);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            response = new ApiResponse(ex, ErrorCode.API_FAILED_UNKNOWN);
-        }
-        return ResponseEntity.ok().body(response);
-    }
+	@PostMapping("/clone")
+	public ResponseEntity<ApiResponse> clone(@RequestBody CloneServiceProgramRequest request) {
+		ApiResponse response;
+		try {
+			ServiceProgram data = serviceProgramService.clone(request);
+			ServiceProgramResponse responseEntity = new ServiceProgramResponse(data);
+			response = new ApiResponse(ApiResponseStatus.SUCCESS.getValue(), responseEntity);
+		} catch (RestApiException ex) {
+			response = new ApiResponse(ex);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			response = new ApiResponse(ex, ErrorCode.API_FAILED_UNKNOWN);
+		}
+		return ResponseEntity.ok().body(response);
+	}
+
 }

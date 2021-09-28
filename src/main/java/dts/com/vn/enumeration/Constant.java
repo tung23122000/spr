2695dea335feb57690ec61b4ca5b345c7a453b97
Dat Nguyen <kod.tq.96@gmail.com>
2021-09-28 20:30:36 +0700

    edit time alert

--- src/main/java/dts/com/vn/enumeration/Constant.java
+++ src/main/java/dts/com/vn/enumeration/Constant.java
@@ -6,7 +6,8 @@ public class Constant {
 
 	public static final String UPLOAD_FILE_FAIL = "upload to server fail";
 
-	// public static long TIME_TO_LIVE_PAGE = 300; // Thời gian 1 phiên chỉnh sửa 1 package service
+	 public static long TIME_TO_LIVE_PAGE = 300; // Thời gian 1 phiên chỉnh sửa 1 package service
+
 	public static final String EXCUTE_QUERY_SUCCESS = "Thực hiện query thành công.";
 
 	public static final String EXCUTE_QUERY_NO_RECORDS = "Thực hiện query thành công. Không có bản ghi nào tồn tại";
@@ -16,8 +17,6 @@ public class Constant {
 	public static String ACTIVE = "ACTIVE";
 
 	public static String PENDING = "PENDING";
-
-	public static long TIME_TO_LIVE_PAGE = 60;
 
 	public static final String DUPLICATE_RECORD = "Không thể thêm mới bản ghi do trùng lặp dữ liệu";
 

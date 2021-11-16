1b8a21240b5a4beccdd759ea146fadc3cb4ef51f
giangdh_dts <giangdh@dts.com.vn>
2021-11-16 20:40:15 +0700

    Add 4 file controller for 4 table

--- src/main/java/dts/com/vn/ilink/constants/IlinkTableName.java
+++ src/main/java/dts/com/vn/ilink/constants/IlinkTableName.java
@@ -4,4 +4,12 @@ public class IlinkTableName {
 
 	public static final String LKT_CHECK_CONDITIONS = "LKT_CHECK_CONDITIONS";
 
+	public static final String LKT_COMMERCIAL_RFS_MAPPING = "LKT_COMMERCIAL_RFS_MAPPING";
+
+	public static final String LKT_PACKAGE_INFO = "LKT_PACKAGE_INFO";
+
+	public static final String LKT_FETCH_PARAMETERS = "LKT_FETCH_PARAMETERS";
+
+	public static final String LKT_SMS_FORMAT = "LKT_SMS_FORMAT";
+
 }
---
+++ src/main/java/dts/com/vn/ilink/controller/LKTFetchParametersController.java
@@ -0,0 +1,12 @@
+package dts.com.vn.ilink.controller;
+
+import dts.com.vn.config.SpringFoxConfig;
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@Api(tags = {SpringFoxConfig.LKT_FETCH_PARAMETERS_TAG})
+public class LKTFetchParametersController {
+}

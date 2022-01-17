d8730c390cf711e0ce2a7602f231e998d204e27f
giangdh <giangdh@dts.com.vn>
2022-01-17 16:14:05 +0700

    Add some log for report controller

--- src/main/java/dts/com/vn/service/ReportService.java
+++ src/main/java/dts/com/vn/service/ReportService.java
@@ -9,16 +9,24 @@ import dts.com.vn.repository.ServiceTypeRepository;
 import dts.com.vn.response.ApiResponse;
 import dts.com.vn.response.DailyReportResponse;
 import dts.com.vn.response.ListPackageResponse;
+import dts.com.vn.util.LogUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
-import java.util.*;
-import java.util.concurrent.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
 @Service
 public class ReportService {
+
+	private static final Logger logger = LoggerFactory.getLogger(ReportService.class);
 
 	private final ServicePackageRepository servicePackageRepository;
 
@@ -39,8 +47,8 @@ public class ReportService {
 		this.sasReTaskParameterRepository = sasReTaskParameterRepository;
 	}
 
-	public ApiResponse dailyReport(Long serviceTypeId, String date){
 
+	public ApiResponse dailyReport(Long serviceTypeId, String date) {
 		ApiResponse response = new ApiResponse();
 		DailyReportResponse data = new DailyReportResponse();
 		List<CompletableFuture<ListPackageResponse>> result = new ArrayList<>();
@@ -49,22 +57,20 @@ public class ReportService {
 		List<ServicePackage> listAllPackageSameGroup = servicePackageRepository.findAllByServiceTypeId(serviceTypeId);
 		Timestamp start = Timestamp.valueOf(date + " " + "00:00:00");
 		Timestamp end = Timestamp.valueOf(date + " " + "23:59:59");
+		long startExcute = System.currentTimeMillis();
 		if (listAllPackageSameGroup.size() > 0) {
-			result = listAllPackageSameGroup.stream().map(servicePackage -> {
-				return CompletableFuture.supplyAsync(() -> {
-					ListPackageResponse listPackageResponse = new ListPackageResponse();
-					Integer numberRecordSuccess = registerRepository
-							.findAllByPackageIdAndRegDate(servicePackage.getPackageId(), start, end);
-					Integer numberRecordFailed = sasReTaskParameterRepository
-							.findAllFailByParameterValueAndDate(servicePackage.getCode(), start, end);
-
-					listPackageResponse.setPackageCode(servicePackage.getCode());
-					listPackageResponse.setPackageName(servicePackage.getName());
-					listPackageResponse.setNumberRecordSuccess(numberRecordSuccess);
-					listPackageResponse.setNumberRecordFailed(numberRecordFailed);
-					return listPackageResponse;
-				});
-			}).collect(Collectors.toList());
+			result = listAllPackageSameGroup.stream().map(servicePackage -> CompletableFuture.supplyAsync(() -> {
+				ListPackageResponse listPackageResponse = new ListPackageResponse();
+				Integer numberRecordSuccess = registerRepository
+						.findAllByPackageIdAndRegDate(servicePackage.getPackageId(), start, end);
+				Integer numberRecordFailed = sasReTaskParameterRepository
+						.findAllFailByParameterValueAndDate(servicePackage.getCode(), start, end);
+				listPackageResponse.setPackageCode(servicePackage.getCode());
+				listPackageResponse.setPackageName(servicePackage.getName());
+				listPackageResponse.setNumberRecordSuccess(numberRecordSuccess);
+				listPackageResponse.setNumberRecordFailed(numberRecordFailed);
+				return listPackageResponse;
+			})).collect(Collectors.toList());
 		}
 		CompletableFuture.allOf(result.toArray(new CompletableFuture[result.size()])).join();
 		data.setListPackage(result.stream().map(future -> {
@@ -75,8 +81,11 @@ public class ReportService {
 			}
 			return null;
 		}).collect(Collectors.toList()));
+		long endExcute = System.currentTimeMillis();
+		LogUtil.writeLog(logger, "time excute function report", endExcute - startExcute + "ms");
 		response.setStatus(200);
 		response.setData(data);
 		return response;
 	}
+
 }

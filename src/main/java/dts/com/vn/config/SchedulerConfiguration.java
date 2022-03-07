86b11a2e9f2b9e981e6ba7c942bc2a646a61d9a3
giangdh <giangdh@dts.com.vn>
2022-03-07 10:43:28 +0700

    Thêm các function lấy data report để insert vào bảng report

--- src/main/java/dts/com/vn/config/SchedulerConfiguration.java
+++ src/main/java/dts/com/vn/config/SchedulerConfiguration.java
@@ -9,13 +9,59 @@ import org.springframework.scheduling.annotation.Scheduled;
 public class SchedulerConfiguration {
 
 	/**
-	 * Description - Cron job query lấy data insert vào bảng report hàng đêm
+	 * Description - 1.Cron job query lấy data Báo cáo tổng hợp gói cước
 	 *
 	 * @author - giangdh
 	 * @created - 07/03/2022
 	 */
 	@Scheduled(cron = "0/15 * * * * *")
-	private void queryDataReport() {
+	private void queryDataReport1() {
+
+	}
+
+	/**
+	 * Description - 2.Cron job query lấy data Báo cáo retry gia hạn gói cước
+	 *
+	 * @author - giangdh
+	 * @created - 07/03/2022
+	 */
+	@Scheduled(cron = "0/15 * * * * *")
+	private void queryDataReport2() {
+
+	}
+
+
+	/**
+	 * Description - 3.Cron job query lấy data Báo cáo về top thuê bao thực hiện lệnh ngày hôm trước
+	 *
+	 * @author - giangdh
+	 * @created - 07/03/2022
+	 */
+	@Scheduled(cron = "0/15 * * * * *")
+	private void queryDataReport3() {
+
+	}
+
+	/**
+	 * Description - 4.Cron job query lấy data Báo cáo gia hạn lỗi ngày hôm trước
+	 *
+	 * @author - giangdh
+	 * @created - 07/03/2022
+	 */
+	@Scheduled(cron = "0/15 * * * * *")
+	private void queryDataReport4() {
+
+	}
+
+
+	/**
+	 * Description - Cron job query lấy data Báo cáo chốt số liệu trên hệ thống VASP theo ngày
+	 *
+	 * @author - giangdh
+	 * @created - 07/03/2022
+	 */
+	@Scheduled(cron = "0/15 * * * * *")
+	private void queryDataReport5() {
 
 	}
 

dfa988ed1e88cc0b363851d7626ec5869f6af4cf
Bui  Tinh <tinhbdt@dts.com.vn>
2022-04-12 07:02:22 +0000

    Done api báo cáo tổng hợp hệ thống vasp

--- src/main/java/dts/com/vn/repository/ServiceTypeRepository.java
+++ src/main/java/dts/com/vn/repository/ServiceTypeRepository.java
@@ -2,6 +2,7 @@ package dts.com.vn.repository;
 
 import dts.com.vn.entities.ServiceType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,8 @@ import java.util.List;
 public interface ServiceTypeRepository extends JpaRepository<ServiceType, Long> {
 
 	List<ServiceType> findByStatusAndDisplayStatus(String status, String displayStatus);
+
+	@Query(nativeQuery= true,value = "SELECT service_type_id FROM service_type ORDER BY service_type_id ASC")
+	List<Long> findListServiceTypeId();
+
 }

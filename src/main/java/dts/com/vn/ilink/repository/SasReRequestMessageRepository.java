2e88cd35487354f11ceaa1039f5c4dc9f815a781
giangdh <giangdh@dts.com.vn>
2022-04-19 23:31:31 +0700

    Thêm clean cho request status = 8 (Partly failed)

--- src/main/java/dts/com/vn/ilink/repository/SasReRequestMessageRepository.java
+++ src/main/java/dts/com/vn/ilink/repository/SasReRequestMessageRepository.java
@@ -14,7 +14,7 @@ public interface SasReRequestMessageRepository extends JpaRepository<SasReReques
     @Query(nativeQuery = true,
             value = "SELECT *  FROM ilink.sas_re_request_message " +
                     "WHERE finished_time > ?1 AND finished_time < ?2 " +
-                    "AND req_status IN (7,9) " +
+                    "AND req_status IN (7,8,9) " +
                     "ORDER BY request_id ASC")
     List<SasReRequestMessage> findAllBeetweenDate(Timestamp startDate, Timestamp endDate);
 

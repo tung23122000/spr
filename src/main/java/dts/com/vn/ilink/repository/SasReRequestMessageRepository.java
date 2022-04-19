710fda7cc2a9aa6d7807c7b62c6015f62d30c397
giangdh <giangdh@dts.com.vn>
2022-04-19 12:32:28 +0700

    Update condition scan request from ilink to clean

--- src/main/java/dts/com/vn/ilink/repository/SasReRequestMessageRepository.java
+++ src/main/java/dts/com/vn/ilink/repository/SasReRequestMessageRepository.java
@@ -13,7 +13,8 @@ public interface SasReRequestMessageRepository extends JpaRepository<SasReReques
 
     @Query(nativeQuery = true,
             value = "SELECT *  FROM ilink.sas_re_request_message " +
-                    "WHERE received_time > ?1 AND received_time < ?2 " +
+                    "WHERE finished_time > ?1 AND finished_time < ?2 " +
+                    "AND req_status IN (7,9) " +
                     "ORDER BY request_id ASC")
     List<SasReRequestMessage> findAllBeetweenDate(Timestamp startDate, Timestamp endDate);
 
